d057e7bc8c9f8eac17b7d13fa921b78cee85db04
Nitesh <iitniteshjammu@gmail.com>
2024-09-23 18:34:09 +0530

    Final Commit

--- src/assets/About.jsx
+++ src/assets/About.jsx
@@ -27,22 +27,22 @@ const About = () => {
     const transformOpac = useTransform(scrollYProgress, [0, 1], ['-10%', '300%']);
 
     return (
-        <div id="About" ref={scrollRef} className="flex flex-wrap justify-center items-center font-mono">
+        <div id="About" ref={scrollRef} className="hidden md:flex flex-wrap justify-center items-center font-mono p-4">
             <motion.div
                 style={isDesktop ? { x: transformX, opacity: transformOpac } : {}}
-                className="flex-1 text-center lg:text-right"
+                className="hidden md:block flex-1 text-center lg:text-right"
             >
                 <p className="text-white">
-                    <b className='text-7xl text-nit-100 hover:text-8xl hover:text-white font-sans transition-all'>About Me</b><br /> <br />
-                    <span className='text-3xl text-nit-100'>
-                        I'm <b className='text-white hover:text-4xl hover:text-nit-100'>Front-end Developer</b> and <br />
-                        <b className='text-white hover:text-4xl hover:text-nit-100'>Chemical Engineer</b> from India. <br /> <br /> <br />
+                    <b className='text-4xl md:text-7xl text-nit-100 hover:text-5xl md:hover:text-8xl hover:text-white font-sans transition-all'>About Me</b><br /> <br />
+                    <span className='text-xl md:text-3xl text-nit-100'>
+                        I'm <b className='text-white hover:text-2xl md:hover:text-4xl hover:text-nit-100'>Front-end Developer</b> and <br />
+                        <b className='text-white hover:text-2xl md:hover:text-4xl hover:text-nit-100'>Chemical Engineer</b> from India. <br /> <br /> <br />
                     </span>
-                    <span className='text-lg ml-[10vw]'>
-                        I enjoy taking complex problems and converting them into simple and beautiful websites. I also love the logic and structure of coding and always strive to write simple and efficient code, whether it be <b className='text-xl'>HTML, CSS or JS.</b>
+                    <span className='text-sm md:text-lg block md:inline ml-0 md:ml-[10vw]'>
+                        I enjoy taking complex problems and converting them into simple and beautiful websites. I also love the logic and structure of coding and always strive to write simple and efficient code, whether it be <b className='text-base md:text-xl'>HTML, CSS or JS.</b>
                     </span>
-                    <span className='text-xl'>
-                        When I am not coding or playing with <b>P&ID</b>, you'll find me in the <b className='text-nit-100 font-sans text-2xl'>Gym.</b>
+                    <span className='text-base md:text-xl block md:inline'>
+                        When I am not coding or playing with <b>P&ID</b>, you'll find me in the <b className='text-nit-100 font-sans text-lg md:text-2xl'>Gym.</b>
                     </span>
                 </p>
             </motion.div>
@@ -50,7 +50,7 @@ const About = () => {
                 <img
                     src={me}
                     alt="me"
-                    className="h-60 md:h-[75vh] mt-[10vh] object-cover shadow-lg m-[10vw]"
+                    className=" md:h-[75vh] mt-[5vh] md:mt-[10vh] object-cover shadow-lg m-[5vw] md:m-[10vw]"
                 />
             </div>
         </div>
